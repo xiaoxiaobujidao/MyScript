fc7a7678f26ef98a3a25fe6fe64182359c5bb8cd
bujidao <bujidao@duck.com>
2024-01-07 19:40:38 +0000

    add warp

---
+++ warp/README.md
@@ -0,0 +1,12 @@
+Both
+```bash
+curl -s https://raw.githubusercontent.com/xiaoxiaobujidao/MyScript/main/warp/debian.sh | bash -
+```
+IPv4 Only VPS
+```bash
+curl -s https://raw.githubusercontent.com/xiaoxiaobujidao/MyScript/main/warp/debian.sh | bash -s 4
+```
+IPv6 Only VPS
+```bash
+curl -s https://raw.githubusercontent.com/xiaoxiaobujidao/MyScript/main/warp/debian.sh | bash -s 6
+```
---
+++ warp/debian.sh
@@ -0,0 +1,27 @@
+# Install lsb
+sudo apt update && sudo apt install lsb-release gpg curl wget -y
+
+# Add cloudflare gpg key
+curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+
+# Add this repo to your apt repositories
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+
+# Install
+sudo apt update && sudo apt install cloudflare-warp -y
+
+yes | warp-cli register
+
+if [ "$1" == "4" ]; then
+    # ipv4-only VPS
+    warp-cli add-excluded-route 0.0.0.0/0
+    echo "precedence ::ffff:0:0/96 100" >>/etc/gai.conf
+elif [ "$1" == "6" ]; then
+    # ipv6-only VPS
+    warp-cli add-excluded-route ::0/0
+    rm /etc/gai.conf
+fi
+
+warp-cli set-mode warp
+warp-cli connect
+warp-cli enable-always-on
