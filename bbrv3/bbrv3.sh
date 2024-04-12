0a0df1b0e848e5d184eee353661d793424590a2d
bujidao <bujidao@duck.com>
2024-04-13 05:43:32 +0800

    Modify Script

--- bbrv3/bbrv3.sh
+++ bbrv3/bbrv3.sh
@@ -20,4 +20,32 @@ net.ipv4.tcp_congestion_control=bbr
 
 EOF
 
+cat >>/etc/sysctl.d/10-enable-bbr.conf <<EOF 
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+
+net.ipv4.tcp_rmem = 4096 87380 33554432
+net.ipv4.tcp_wmem = 4096 65536 33554432
+
+net.ipv4.tcp_mtu_probing=1
+EOF
+
+
+cat >>/etc/sysctl.conf <<EOF 
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+
+net.ipv4.tcp_rmem = 4096 87380 33554432
+net.ipv4.tcp_wmem = 4096 65536 33554432
+
+net.ipv4.tcp_mtu_probing=1
+EOF
+
+
 reboot
